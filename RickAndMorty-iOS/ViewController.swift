15a682e4c7c2568bbfcbbdcc4e669540ebc8adc5
raymond gitonga <raytosh95@gmail.com>
2019-12-29 02:34:22 +0300

    Fetch and display character names in uitable

--- RickAndMorty-iOS/ViewController.swift
+++ RickAndMorty-iOS/ViewController.swift
@@ -10,21 +10,68 @@ import UIKit
 import Alamofire
 import SwiftyJSON
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return id.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell")
+        cell?.textLabel?.text = name[indexPath.row]
+        
+        return cell!
+    }
+    
+    
+    var id  = [String]()
+    var name = [String]()
+    var status = [String]()
+    var gender = [String]()
+    var species = [String]()
 
+    @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
         
-        let url = "https://rickandmortyapi.com/api/character/"
+        fetchData()
         
+    }
+
+    func fetchData(){
+         let url = "https://rickandmortyapi.com/api/character/"
         
-        Alamofire.request(url, method: .get).responseJSON { (response) in
+       Alamofire.request(url, method: .get).responseJSON { (response) in
             switch response.result{
             case .success:
-                print(response.result)
                 
                 let result = try? JSON(data: response.data!)
-                print(result)
+                let resultArray = result!["results"]
+                
+                self.id.removeAll()
+                self.name.removeAll()
+                self.status.removeAll()
+                self.gender.removeAll()
+                self.species.removeAll()
+                
+                for i in resultArray.arrayValue {
+                    let id = i["id"].stringValue
+                    self.id.append(id)
+                    
+                    let name = i["name"].stringValue
+                    self.name.append(name)
+                    
+                    let status = i["status"].stringValue
+                    self.status.append(status)
+                    
+                    let gender = i["gender"].stringValue
+                    self.gender.append(gender)
+                    
+                    let species = i["species"].stringValue
+                    self.species.append(species)
+                }
+                self.tableView.reloadData()
                 break
             case .failure:
                 print(response.error!)
@@ -32,7 +79,6 @@ class ViewController: UIViewController {
             }
         }
     }
-
 
 }
 
